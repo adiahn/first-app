17f56123901b0db2f2e8f46a5fa60c77921229c8
Adiahn <adnanmukhtar2321@gmail.com>
2025-03-06 15:53:43 +0100

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My new app</Text>
+      <Text>My ne </Text>
       <StatusBar style="auto" />
     </View>
   );
